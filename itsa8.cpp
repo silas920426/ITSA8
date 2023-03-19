0e6380ccc521dc8e8ce686a3dac2258d8ca727b7
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-19 22:23:52 +0800

    加入專案檔案。

---
+++ itsa8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>  
+using namespace std;
+int main()
+{
+    int a, b;
+    while (cin >> a) {
+        for (b = 2; b < a; b++) {
+            if (a % b == 0) {
+                cout << "NO" << endl;
+                break;
+            }
+        }
+        if (a == b) {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
